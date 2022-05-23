0039d28028602111dd266fa5b2513ca81a41c78d
gustavo <fgustavo.n@gmail.com>
2022-05-23 11:53:58 -0300

    Alteração dos arquivos iniciais

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Button from './components/Button';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Input from './components/Input';
+import Title from './components/Title';
+import './components/styles.css'
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Title />
+      <Input />
+      <Button />
+      <Footer />
     </div>
   );
 }
